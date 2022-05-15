94085f841fa2bbc63a9596d079d21124035cb8a1
giselle-ferreira <giselle.fs@gmail.com>
2022-05-15 19:51:08 -0300

    corrections on the server

--- server.js
+++ server.js
@@ -1,12 +1,19 @@
 const express = require('express');
+const path = require('path')
+const fileURLToPath = require('url')
 
+
+const __filename = fileURLToPath(import.meta.url);
+
+const __dirname = path.dirname(__filename)
+
+// instantiating the app
 const app = express();
 
 // Project name
 const appName = 'insta-newversion';
 
 // where build will generate the files
-const outputPath = `${___dirname}/dist/${appName}`
 
 // setting build file to serve the angular content
 app.use(express.static(outputPath));
