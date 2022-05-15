828cf0f2953fb9a278c72d57571c7d7bd8f52138
giselle-ferreira <giselle.fs@gmail.com>
2022-05-15 19:54:46 -0300

    corrections on the server

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const path = require('path')
 const fileURLToPath = require('url')
 
 
-const __filename = fileURLToPath(import.meta.url);
+// const __filename = fileURLToPath(import.meta.url);
 
 const __dirname = path.dirname(__filename)
 
