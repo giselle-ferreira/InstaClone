5e4ebf2629f7e59df09fd95f11e8dbe7e374496c
giselle-ferreira <giselle.fs@gmail.com>
2022-05-15 20:39:37 -0300

    config for deploy

--- server.js
+++ server.js
@@ -2,11 +2,6 @@ const express = require('express');
 const path = require('path')
 const fileURLToPath = require('url')
 
-
-// const __filename = fileURLToPath(import.meta.url);
-
-// const __dirname = path.dirname(__filename)
-
 // instantiating the app
 const app = express();
 
